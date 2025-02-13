6f8a3fc9555e5f3c154e6ac9f4db9eee055d7b97
Yair Vogel <yairvogel@github.com>
2025-02-13 18:22:29 +0100

    shell - add output redirect to file

--- shell/main.c
+++ shell/main.c
@@ -1,9 +1,11 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <string.h>
+#include <assert.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #define UNUSED(x) (void)(x)
 
@@ -17,7 +19,8 @@ typedef struct {
 KeyValuePair variables[100] = { 0 };
 
 int assignVariable(char* var, char* val, KeyValuePair variables[100]);
-char* getVariable(char* var, KeyValuePair variables[100]);
+char* getVariable(char* const var, KeyValuePair variables[100]);
+__pid_t execcmd(char* tokens[], char* outfile);
 
 int main(int argc, char* argv[]) {
   UNUSED(argc);
@@ -50,6 +53,8 @@ int main(int argc, char* argv[]) {
     int len;
     for (len = 0; len < 511; len++) {
       if (tok == NULL) break;
+
+      // parameter expansion
       if (tok[0] == '$') {
         char* val = getVariable(tok+1, variables);
         if (val == NULL) {
@@ -59,42 +64,68 @@ int main(int argc, char* argv[]) {
         }
       }
 
-      *(tokens + len) = tok;
+      tokens[len] = tok;
       tok = strtok(NULL, " \n");
     }
 
-    *(tokens + len + 1) = NULL;
-    
+    tokens[len] = NULL;
     if (len == 0) {
       lastStatus = 0;
       continue;
     }
 
-    // tokens holds NULL terminated array of char* 
-    int cid = fork();
-    if (cid < 0) {
-      fprintf(stderr, "failed forking executable\n");
+    if (len == 1 && (strcmp(tokens[0], "exit") == 0)) {
+      exit(0);
+    }
+    
+    char* outfile = NULL;
+    if (len > 2 && strcmp(tokens[len-2], ">") == 0) {
+      outfile = tokens[len-1];
+      tokens[len-2] = NULL;
     }
 
-    else if (cid == 0) {
-      errno = 0;
-      execvp(*tokens, tokens);
-      perror("failed execvp");
+    int cid = execcmd(tokens, outfile);
+    int status;
+    if (waitpid(cid, &status, 0) == -1) {
+      perror("waitpid falied");
       exit(EXIT_FAILURE);
-    } else {
-      int status;
-      if (waitpid(cid, &status, 0) == -1) {
-        perror("waitpid falied");
-        exit(EXIT_FAILURE);
-      }
-      
-      if (WIFEXITED(status)) {
-        lastStatus = WEXITSTATUS(status);
-      }
+    }
+
+    if (WIFEXITED(status)) {
+      lastStatus = WEXITSTATUS(status);
     }
   }
 
   return 0;
+}
+
+mode_t newFileMode =  S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+
+int execcmd(char* tokens[], char* outfile) {
+  errno = 0;
+  int cid = fork();
+  if (cid < 0) {
+    perror("fork failed");
+    exit(EXIT_FAILURE);
+  }
+  else if (cid == 0) {
+    if (outfile != NULL) {
+      errno = 0;
+      int fd = open(outfile, O_WRONLY | O_CREAT, newFileMode);
+      if (fd < 0) {
+        perror("open failed");
+        exit(EXIT_FAILURE);
+      }
+      dup2(fd, 1);
+      close(fd);
+    }
+
+    execvp(*tokens, tokens);
+    perror("failed execvp");
+    exit(EXIT_FAILURE);
+  } else {
+    return cid;
+  }
 }
 
 int assignVariable(char* var, char* val, KeyValuePair variables[100]) {
@@ -121,13 +152,11 @@ int assignVariable(char* var, char* val, KeyValuePair variables[100]) {
 char* getVariable(char* var, KeyValuePair variables[100]) {
   for (int i = 0; i < 100; i++) {
     KeyValuePair* kvp = variables + i;
-    if (kvp->key == NULL) {
+    if (kvp->key == NULL)
       return NULL;
-    }
 
-    if (strncmp(kvp->key, var, BUFLEN) == 0) {
+    if (strncmp(kvp->key, var, BUFLEN) == 0)
       return kvp->value;
-    }
   }
 
   return NULL;
